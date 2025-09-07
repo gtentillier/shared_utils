ab05a6e06a36ab0f4256a9cda218d547792e5ae9
gtentillier <gtentillier@orange.fr>
2025-09-07 19:13:50 +0200

    Add initial implementation of shared_utils package with architecture and path utilities

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .get_architecture import generate_architecture
+from .get_paths import path_data, path_project
+
+__all__ = ["path_data", "path_project", "generate_architecture"]
---
+++ src/get_paths.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from typing import Optional
+
+path_project = (Path(__file__).resolve().parents[2])
+
+
+def path_data(filename: Optional[str] = None) -> Path:
+    """Get the path to the data directory, optionally appending a filename.
+    Args:
+        filename (Optional[str]): The name of the file to append to the data path.
+    Returns:
+        Path: The full path to the data directory or the specified file within it.
+    """
+    if filename is None:
+        return path_project / 'data'
+    else:
+        return path_project / 'data' / filename
+
+
+__all__ = ["path_project", "path_data"]
